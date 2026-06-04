5ff38af3fbde0bf488a5e829268d339f54ad3f69
Flor Stellamans <florstellamamans@gmail.com>
2026-06-04 11:58:21 +0200

    add send and read functions in UsbConnector class

--- Altec.Api/Altec.Api/Domain/Printers/UsbConnector.cs
+++ Altec.Api/Altec.Api/Domain/Printers/UsbConnector.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Altec.Api.Domain.Printers;
 
 public class UsbConnector
@@ -18,20 +20,31 @@ public class UsbConnector
 
     public void Send(string command)
     {
-        // convert string into bytes[]
-        // send the command in bytes to the file
-        // FileStream.Send(bytes[])
-        // if success return ok else return exception
+        var bytes = Encoding.ASCII.GetBytes(command);
+        
+        try
+        {
+            _stream.Write(bytes, 0, bytes.Length);
+        }
+        catch (IOException ex)
+        {
+            throw new IOException("Unable to send command to printer.", ex);
+        }
     }
 
     public string Read()
     {
-        var result = "";
+        var buffer = new byte[1024];
 
-        // read the data from the file
-        // convert to string
-        // return the result
-
-        return result;
+        try
+        {
+            var bytesRead = _stream.Read(buffer, 0, buffer.Length);
+            var result = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            return result;
+        }
+        catch (IOException ex)
+        {
+            throw new IOException("Unable to read data from the printer", ex);
+        }
     }
 }
